0af9f5e8ba6474be36e1df43d1f9098d72fc6f6b
YoungjinLee <117518123+20zin@users.noreply.github.com>
2024-08-25 17:00:08 +0900

    6 commit

--- src/test/java/com/example/transactional/propagation/BasicTxTest.java
+++ src/test/java/com/example/transactional/propagation/BasicTxTest.java
@@ -48,4 +48,71 @@ public class BasicTxTest {
         log.info("트랜잭션 롤백 완료");
     }
 
+    @Test
+    void double_commit(){
+
+        log.info("트랜잭션1 시작");
+        TransactionStatus tx1 = txManager.getTransaction(new DefaultTransactionAttribute());
+        log.info("트랜잭션1 커밋");
+        txManager.commit(tx1);
+
+        log.info("트랜잭션2 시작");
+        TransactionStatus tx2 = txManager.getTransaction(new DefaultTransactionAttribute());
+        log.info("트랜잭션2 커밋");
+        txManager.commit(tx2); //1번이 커밋하면 2번이 커밋되고...
+    }
+
+    @Test
+    void double_commit_rollback(){
+
+        log.info("트랜잭션1 시작");
+        TransactionStatus tx1 = txManager.getTransaction(new DefaultTransactionAttribute());
+        log.info("트랜잭션1 커밋");
+        txManager.commit(tx1);
+
+        log.info("트랜잭션2 시작");
+        TransactionStatus tx2 = txManager.getTransaction(new DefaultTransactionAttribute());
+        log.info("트랜잭션2 롤백");
+        txManager.rollback(tx2); //1번이 커밋하면 2번이 커밋되고...
+    }
+
+    @Test
+    void inner_commit(){
+        log.info("외부 트랜잭션 시작");
+        TransactionStatus outer = txManager.getTransaction(new DefaultTransactionAttribute());
+        log.info("outer.isNewTransaction() = {}", outer.isNewTransaction()); //처음 시작되는 tx
+
+        inner();
+
+        log.info("외부 트랜잭션 커밋");
+        txManager.commit(outer);
+    } //하나의 물리 트랜잭션으로 묶임 (각각은 논리 트랜잭션)
+    //외부 트랜잭션만 물리 트랜잭션을 시작하고 커밋한다
+
+
+    @Test
+    void outer_rollback(){
+        log.info("외부 트랜잭션 시작");
+        TransactionStatus outer = txManager.getTransaction(new DefaultTransactionAttribute());
+
+        log.info("outer.isNewTransaction() = {}", outer.isNewTransaction()); //처음 시작되는 tx
+
+        inner();
+
+        log.info("외부 트랜잭션 롤백");
+        txManager.rollback(outer);
+    } //하나의 물리 트랜잭션으로 묶임 (각각은 논리 트랜잭션)
+    //외부 트랜잭션만 물리 트랜잭션을 시작하고 커밋한다
+
+    private void inner() {
+        log.info("내부 트랜잭션 시작");
+        TransactionStatus inner = txManager.getTransaction(new DefaultTransactionAttribute());
+        log.info("outer.isNewTransaction() = {}", inner.isNewTransaction()); //외부에서 실행하는 도중 내부tx실헹
+        log.info("내부 트랜잭션 커밋");
+        txManager.commit(inner);
+    }
+
+
+
+
 }
